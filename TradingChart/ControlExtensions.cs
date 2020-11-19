e73678183991361a91a2534368090ea7b4238212
Yusuke Shibata <yooce.k@gmail.com>
2020-11-19 13:19:44 +0900

    クリップボードコピー追加

--- TradingChart/ControlExtensions.cs
+++ TradingChart/ControlExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,26 @@ namespace MagicalNuts
 		public static void AlignTop(this Control me, Control baseCtrl)
 		{
 			me.Top = baseCtrl.Top + baseCtrl.Height + baseCtrl.Margin.Bottom + me.Margin.Top;
+		}
+
+		public static void CopyToClipboard(this Control me, double zoom)
+		{
+			// キャプチャ
+			Bitmap bmp = new Bitmap(me.Width, me.Height);
+			me.DrawToBitmap(bmp, new Rectangle(0, 0, me.Width, me.Height));
+
+			// 拡縮
+			Bitmap canvas = new Bitmap((int)(me.Width * zoom), (int)(me.Height * zoom));
+			Graphics g = Graphics.FromImage(canvas);
+			g.DrawImage(bmp, 0, 0, (int)(bmp.Width * zoom), (int)(bmp.Height * zoom));
+
+			// クリップボードにコピー
+			Clipboard.SetImage(canvas);
+
+			// 終了
+			g.Dispose();
+			canvas.Dispose();
+			bmp.Dispose();
 		}
 	}
 }
