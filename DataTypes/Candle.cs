2724fa3bd884b94ae427f3ff3fa7a1cfd3c0fc7b
Yusuke Shibata <yooce.k@gmail.com>
2020-11-24 11:41:26 +0900

    DataTypes追加

---
+++ DataTypes/Candle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MagicalNuts.DataTypes
+{
+	public class Candle
+	{
+		public string Code { get; set; }
+		public DateTime DateTime { get; set; }
+		public decimal Open { get; set; }
+		public decimal High { get; set; }
+		public decimal Low { get; set; }
+		public decimal Close { get; set; }
+		public long Volume { get; set; }
+
+		public static int Compare(Candle a, Candle b)
+		{
+			if (a.DateTime < b.DateTime) return -1;
+			if (a.DateTime == b.DateTime) return 0;
+			return 1;
+		}
+	}
+}
--- TradingChartSample/Form1.Designer.cs
+++ TradingChartSample/Form1.Designer.cs
@@ -29,13 +29,36 @@ namespace TradingChartSample
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.tradingChart1 = new MagicalNuts.TradingChart();
+			((System.ComponentModel.ISupportInitialize)(this.tradingChart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tradingChart1
+			// 
+			this.tradingChart1.CandleTerm = MagicalNuts.CandleTerm.Dayly;
+			this.tradingChart1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tradingChart1.Location = new System.Drawing.Point(0, 0);
+			this.tradingChart1.Name = "tradingChart1";
+			this.tradingChart1.Size = new System.Drawing.Size(1008, 537);
+			this.tradingChart1.TabIndex = 0;
+			this.tradingChart1.Text = "tradingChart1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1008, 537);
+			this.Controls.Add(this.tradingChart1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.tradingChart1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private MagicalNuts.TradingChart tradingChart1;
 	}
 }
 
